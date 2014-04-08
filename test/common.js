d81ac1485aa6e26596ba7ca8bc637ea49c75258b
Sebastien Dumetz <sebastien.dumetz@isen-lille.fr>
2014-04-08 15:43:37 +0200

    added structure for a test suite

---
+++ test/common.js
@@ -0,0 +1,4 @@
+//TEST FRAMEWORKS
+var chai = require('chai');
+
+global.expect = chai.expect;
---
+++ test/test_sample.js
@@ -0,0 +1,6 @@
+describe('sample test',function() {
+	it('check everything is fine',function(){
+		var imFine = true;
+		expect(imFine).to.be.true;
+	})
+})
