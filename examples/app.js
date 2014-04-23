813c095a05e05344312c426bf0ecfcb899d88437
Sebastien Dumetz <sebastien.dumetz@isen-lille.fr>
2014-04-23 17:48:22 +0200

    refctored example app

--- examples/app.js
+++ examples/app.js
@@ -6,133 +6,39 @@ var VLCevent = new EventEmitter();
 var vlc = new Vlc([
   '-I', 'dummy',
   '--no-video-title-show',
+  '--logmode','syslog',
+  '--syslog',
+  '--syslog-facility','local7',
+
 ]);
+var media = vlc.mediaFromFile(process.argv[2]);
+media.parseSync();
 
-	var rl = readline.createInterface({
-		input: process.stdin,
-	  	output: process.stdout
-	})
+media.track_info.forEach(function (info) {
+  console.log(info);
+});
 
-function Choose_Video(lien, pos)
-{
-	var media = vlc.mediaFromFile(lien);//ici le lien de la vidéo. mediaFromFile est une fonction dans le fichier vlc.js
-	media.parseSync();
+console.log(media.artist, '--', media.album, '--', media.title);
 
-	media.track_info.forEach(function (info) {
-	  console.log(info);
-	});
+var player = vlc.mediaplayer;
+player.media = media;
+console.log('Media duration:', media.duration);
 
-	console.log(media.artist, '--', media.album, '--', media.title);
-
-	var player = vlc.mediaplayer;
-	player.media = media;
-	console.log('Media duration:', media.duration);
-
-	player.play();
-	var POS = pos;
-	player.position = POS;
+player.play();
 
 
+player.on('Paused',function(){
+	console.log("paused");
+});
+player.on('EncounteredError',function(){
 
-	return {'1':player, '2':media};
-
-}
-
-
-function Video_Paused(player, media, POS)
-{
-	player.on('Paused',function(){
-	    	PausedQuest(player, media, POS);
-	});
-}
-
-function Video_Playing(player, media)
-{
-	player.on('Playing',function(){
-		SecondQuest(player, media);
-	});
-}
+	console.log(vlc.errmsg());
+	console.log("error")
+})
 
 
-
-function SecondQuest(player, media)
-{
-	rl.question("type q to exit or p to pause: ", function(answer) {
-		if(answer=="q"){
-			 player.stop();
-			 media.release();
-			 vlc.release();
-			 rl.close();
-		}
-		else if (answer=="p"){
-			player.pause();
-		
-		}
-		else {
-			console.log("\nYou're answer is not correct please try again!!");
-			SecondQuest(player,media);
-		}
-	 
-	});
-}
-
-function PausedQuest(player, media, POS)
-{
-	rl.question('type : \n q to exit \n b to return back to the Menu '+
-		'\n c to continue playing \n r to replay \n ===> ', function(answer) {
-			ans=answer;
-			if(answer=="q"){
-				 player.stop();
-				 media.release();
-				 vlc.release();
-				 rl.close();
-			}
-			else if (answer=="r"){
-			   player.stop();
-			   player.play();
-			}
-			else if (answer=="c"){
-			   player.play();
-			}
-			else if (answer == "b"){
-			   Menu();
-			}
-			else{
-			   console.log("\nYou're answer is not correct please try again!!");
-			   PausedQuest(player, media, POS);
-			}
-		});
-}
-
-
-
-function Menu()
-{
-   rl.question("\t\t\tMENU \ntype 1 or 2 to see each video or q to exit : ", function(answer) {
-	   switch (answer){
-		case '1':
-			var Dico = Choose_Video('/home/khamaily/Stage Holusion/mars.mp4',0.0);
-			Video_Paused(Dico['1'], Dico['2'], 0.0);
-			Video_Playing(Dico['1'], Dico['2'], 0.0);
-			SecondQuest(Dico['1'], Dico['2']);
-			break;
-		case 'q':
-			vlc.release();
-			rl.close();
-			break;
-		case '2':
-			var Dico = Choose_Video('/home/khamaily/Stage Holusion/test.mp4',0.0);
-			Video_Paused(Dico['1'], Dico['2'], 0.0);
-			Video_Playing(Dico['1'], Dico['2'], 0.0);
-			SecondQuest(Dico['1'], Dico['2']);
-			break;
-		default:
-			console.log("\nYou're answer is not correct please try again!!");
-			Menu();
-			break;
-	   }
-   });
-}
-
-Menu();
-
+setTimeout(function(){
+	player.stop();
+    media.release();
+    vlc.release();
+},10000)
